f36960f062b5adb94b807b7558bdd64a46bdb628
Winter0er <52900754+Winter0er@users.noreply.github.com>
2020-02-25 18:59:46 +0700

    Bonus home activity + navigation listioner

--- app/src/main/java/com/example/lmemo_capstone_project/view/HomeActivity.java
+++ app/src/main/java/com/example/lmemo_capstone_project/view/HomeActivity.java
@@ -1,15 +1,21 @@
 package com.example.lmemo_capstone_project.view;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.lmemo_capstone_project.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
     DrawerLayout mDrawerLayout;
     ActionBarDrawerToggle mDrawerToggle;
     @Override
@@ -24,5 +30,97 @@ public class HomeActivity extends AppCompatActivity {
 
         mDrawerLayout.addDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        DefaultFragment();
+
     }
+
+    //Default cho man hinh home la search
+    public void DefaultFragment(){
+        SearchFragment fragment = new SearchFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrameLayout,fragment,"searchHome");
+        WordSearchingFragment wordFragment = new WordSearchingFragment();
+        fragmentTransaction.replace(R.id.searchFrameLayout,wordFragment,"SearchWord");
+        fragmentTransaction.commit();
+
+    }
+
+    //Set item listener tren navigation
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.Search_Home){
+            DefaultFragment();
+        }
+        else if(id == R.id.login){
+            SignInFragment fragment = new SignInFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainFrameLayout,fragment,"signIn");
+            fragmentTransaction.commit();
+        }
+        else if(id == R.id.createAcc){
+
+        }
+        else if(id == R.id.myAccount){
+            MyAccountFragment fragment = new MyAccountFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainFrameLayout,fragment,"myAccount");
+            fragmentTransaction.commit();
+        }
+        else if(id == R.id.myFlashcard){
+            FlashCardFragment fragment = new FlashCardFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainFrameLayout,fragment,"flashCard");
+            fragmentTransaction.commit();
+        }
+        else if(id == R.id.myNote){
+            NotesFragment fragment = new NotesFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainFrameLayout,fragment,"Notes");
+            fragmentTransaction.commit();
+        }
+        else if(id == R.id.contact){
+            ContactUsFragment fragment = new ContactUsFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainFrameLayout,fragment,"Contact");
+            fragmentTransaction.commit();
+        }
+        else if(id == R.id.newInfo){
+            WhatsNewFragment fragment = new WhatsNewFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainFrameLayout,fragment,"whatNew");
+            fragmentTransaction.commit();
+        }
+        else if(id == R.id.terms){
+            TermsofUseFragment fragment = new TermsofUseFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainFrameLayout,fragment,"termsOfUse");
+            fragmentTransaction.commit();
+        }
+        else if(id == R.id.privacy){
+            PrivacyFragment fragment = new PrivacyFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainFrameLayout,fragment,"privacy");
+            fragmentTransaction.commit();
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+
 }
