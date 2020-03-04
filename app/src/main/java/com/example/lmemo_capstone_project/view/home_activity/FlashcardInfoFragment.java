587369a3ab1eb2c4667083c2f90b2a5973b3c19e
Winter0er <52900754+Winter0er@users.noreply.github.com>
2020-03-04 21:14:38 +0700

    Revert "UI for UC:22"
    This reverts commit 192fa15831fba25a6047207f4b7aeba2e3e6a381.

--- app/src/main/java/com/example/lmemo_capstone_project/view/home_activity/FlashcardInfoFragment.java
+++ app/src/main/java/com/example/lmemo_capstone_project/view/home_activity/FlashcardInfoFragment.java
@@ -2,7 +2,6 @@ package com.example.lmemo_capstone_project.view.home_activity;
 
 import android.os.Bundle;
 
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -21,7 +20,7 @@ import com.example.lmemo_capstone_project.model.room_db_entity.Word;
 public class FlashcardInfoFragment extends Fragment {
 
     private Word[] flashcardWord;
-    CardView cvClose;
+
     public FlashcardInfoFragment() {
         // Required empty public constructor
     }
@@ -35,15 +34,8 @@ public class FlashcardInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        final View view = inflater.inflate(R.layout.fragment_flashcard_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_flashcard_info, container, false);
         wordSearchResult(view);
-        cvClose = (CardView)view.findViewById(R.id.cvClose);
-        cvClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().onBackPressed();
-            }
-        });
 //        getActivity().onBackPressed();
         return view;
     }
