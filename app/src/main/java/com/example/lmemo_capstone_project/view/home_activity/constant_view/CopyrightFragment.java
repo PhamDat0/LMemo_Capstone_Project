8320b5d95975115976876bd8ba6784c6b3235457
Winter0er <52900754+Winter0er@users.noreply.github.com>
2020-04-21 13:09:05 +0700

    update

--- app/src/main/java/com/example/lmemo_capstone_project/view/home_activity/constant_view/CopyrightFragment.java
+++ app/src/main/java/com/example/lmemo_capstone_project/view/home_activity/constant_view/CopyrightFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,19 +14,18 @@ import com.example.lmemo_capstone_project.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ContactUsFragment extends Fragment {
-    Button b1;
+public class CopyrightFragment extends Fragment {
 
-    public ContactUsFragment() {
+
+    public CopyrightFragment() {
         // Required empty public constructor
-
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+        View view = inflater.inflate(R.layout.fragment_copyright, container, false);
         return view;
     }
 
