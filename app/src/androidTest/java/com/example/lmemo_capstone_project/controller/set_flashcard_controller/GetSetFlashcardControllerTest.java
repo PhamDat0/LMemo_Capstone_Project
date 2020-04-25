b22c4865ff8220b80557c53619e90e3d36cf3830
Hoang-Khai <khainh1998@gmail.com>
2020-04-25 22:37:25 +0700

    Fix next page cause duplicated

--- app/src/androidTest/java/com/example/lmemo_capstone_project/controller/set_flashcard_controller/GetSetFlashcardControllerTest.java
+++ app/src/androidTest/java/com/example/lmemo_capstone_project/controller/set_flashcard_controller/GetSetFlashcardControllerTest.java
@@ -1,12 +1,53 @@
 package com.example.lmemo_capstone_project.controller.set_flashcard_controller;
 
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.lmemo_capstone_project.model.room_db_entity.SetFlashcard;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class GetSetFlashcardControllerTest {
-    private final String NAME_EXPECTED_1 = "dat's set";
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class GetSetFlashcardControllerTest {
+    private final int NUMBER_OF_SET_EXPECTED = 2;
+    private final String NAME_EXPECTED_1 = "dat's set";
+    private final String NAME_EXPECTED_2 = "聞く";
+    private final String USER_ID_EXPECTED_1 = "J4seeIS7j7d7wmbi1rfuuB6AyYv2";
+    private final String USER_ID_EXPECTED_2 = "WRNc0f75K9hBWiNEPeAO6oEF6n02";
+    private final int NUMBER_OF_FC_EXPECTED_1 = 11;
+    private final int NUMBER_OF_FC_EXPECTED_2 = 1;
+
+    private GetSetFlashcardController getSetFlashcardController;
+
+    @Before
+    public void initTestController() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        getSetFlashcardController = new GetSetFlashcardController(appContext);
+    }
 
     @Test
     public void getOfflineSet() {
+        List<SetFlashcard> offlineSet = getSetFlashcardController.getOfflineSet();
+        SetFlashcard setFlashcard1 = new SetFlashcard();
+        SetFlashcard setFlashcard2 = new SetFlashcard();
+        try {
+            assertEquals(offlineSet.size(), NUMBER_OF_SET_EXPECTED);
+            setFlashcard1 = offlineSet.get(0);
+            setFlashcard2 = offlineSet.get(1);
+        } catch (Exception e) {
+            fail();
+        }
+        assertEquals(setFlashcard1.getSetName(), NAME_EXPECTED_1);
+        assertEquals(setFlashcard2.getSetName(), NAME_EXPECTED_2);
+        assertEquals(setFlashcard1.getCreatorID(), USER_ID_EXPECTED_1);
+        assertEquals(setFlashcard2.getCreatorID(), USER_ID_EXPECTED_2);
+        assertEquals(setFlashcard1.getWordID().size(), NUMBER_OF_FC_EXPECTED_1);
+        assertEquals(setFlashcard2.getWordID().size(), NUMBER_OF_FC_EXPECTED_2);
     }
 }
--- app/src/main/java/com/example/lmemo_capstone_project/view/home_activity/constant_view/SettingsFragment.java
+++ app/src/main/java/com/example/lmemo_capstone_project/view/home_activity/constant_view/SettingsFragment.java
@@ -1,17 +1,13 @@
 package com.example.lmemo_capstone_project.view.home_activity.constant_view;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -24,7 +20,7 @@ import com.example.lmemo_capstone_project.controller.setting_controller.SettingC
 import com.example.lmemo_capstone_project.controller.word_of_day_controller.WordOfTheDayController;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -52,30 +48,25 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-        dailyWordSwitch = view.findViewById(R.id.dailyWordIsOn);
-        dailyWordHour = view.findViewById(R.id.dailyWordHour);
-        dailyWordMin = view.findViewById(R.id.dailyWordMinute);
-        reminderSwitch = view.findViewById(R.id.reminderIsOn);
-        reminderHour = view.findViewById(R.id.reminderHour);
-        reminderMin = view.findViewById(R.id.reminderMinute);
-        btnSave = view.findViewById(R.id.btnSaveSetting);
-        btnCancel = view.findViewById(R.id.btnCancelSetting);
-        dailyWordDismissSwitch = view.findViewById(R.id.dailyWordDisMissIsOn);
-        reminderDismissSwitch = view.findViewById(R.id.reminderDisMissIsOn);
+        setRef(view);
         final SettingController settingController = new SettingController();
-        settingController.setDailyWordSwitch(getContext(), dailyWordSwitch);
-        settingController.setReminderSwitch(getContext(), reminderSwitch);
-        settingController.addValueToTimeSpinner(dailyWordHour, dailyWordMin, getContext());
-        settingController.addValueToTimeSpinner(reminderHour, reminderMin, getContext());
-        settingController.setDailyWordTimeSpinner(getContext(), dailyWordHour, dailyWordMin);
-        settingController.setReminderTimeSpinner(getContext(), reminderHour, reminderMin);
-        settingController.setDailyWordDismissSwitch(getContext(), dailyWordDismissSwitch);
-        settingController.setReminderDismissSwitch(getContext(), reminderDismissSwitch);
+        setDailyWordSwitch(getContext(), dailyWordSwitch);
+        setReminderSwitch(getContext(), reminderSwitch);
+        addValueToTimeSpinner(dailyWordHour, dailyWordMin, getContext());
+        addValueToTimeSpinner(reminderHour, reminderMin, getContext());
+        setDailyWordTimeSpinner(getContext(), dailyWordHour, dailyWordMin);
+        setReminderTimeSpinner(getContext(), reminderHour, reminderMin);
+        setDailyWordDismissSwitch(getContext(), dailyWordDismissSwitch);
+        setReminderDismissSwitch(getContext(), reminderDismissSwitch);
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                settingController.controlOnClickEvent(getContext(), dailyWordSwitch, dailyWordDismissSwitch, dailyWordHour, dailyWordMin,
-                        reminderSwitch, reminderDismissSwitch, reminderHour, reminderMin);
+                settingController.saveSettingToSharePreferences(getContext(), dailyWordSwitch.isChecked(),
+                        dailyWordDismissSwitch.isChecked(), dailyWordHour.getSelectedItem().toString(),
+                        dailyWordMin.getSelectedItem().toString(), reminderSwitch.isChecked(),
+                        reminderDismissSwitch.isChecked(), reminderHour.getSelectedItem().toString(),
+                        reminderMin.getSelectedItem().toString());
+                Toast.makeText(getContext(), "Save Setting Successful!", Toast.LENGTH_LONG).show();
                 flashcardReminderController = new FlashcardReminderController();
                 flashcardReminderController.createNotificationChannel(getActivity());
                 flashcardReminderController.startAlarm(SharedPreferencesController.reminderIsOn(getActivity().getApplicationContext()), true, getActivity(),
@@ -88,6 +79,83 @@ public class SettingsFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    private void setReminderDismissSwitch(Context context, Switch reminderDismissSwitch) {
+        // set reminder switch  dismiss is checked or not when loading UI
+        reminderDismissSwitch.setChecked(SharedPreferencesController.dismissReminderIsOn(context));
+    }
+
+    private void setDailyWordDismissSwitch(Context context, Switch dailyWordDismissSwitch) {
+        //set daily word switch dismiss is checked or not when load UI
+        dailyWordDismissSwitch.setChecked(SharedPreferencesController.dismissDailyWordIsOn(context));
+    }
+
+    private void setReminderTimeSpinner(Context context, Spinner reminderHour, Spinner reminderMin) {
+        //set time of reminder time spinner when loading UI
+        long time = SharedPreferencesController.getReminderTime(context);
+        Date date = new Date(time);
+        int hour = date.getHours();
+        int minute = date.getMinutes();
+        reminderHour.setSelection(((ArrayAdapter<String>) reminderHour.getAdapter()).getPosition("" + hour));
+        reminderMin.setSelection(((ArrayAdapter<String>) reminderMin.getAdapter()).getPosition("" + minute));
+    }
+
+    private void setDailyWordTimeSpinner(Context context, Spinner dailyWordHour, Spinner dailyWordMin) {
+        // set time of daily word time spinner when loading UI
+        long time = SharedPreferencesController.getDailyWordTime(context);
+        Date date = new Date(time);
+        int hour = date.getHours();
+        int minute = date.getMinutes();
+        dailyWordHour.setSelection(((ArrayAdapter<String>) dailyWordHour.getAdapter()).getPosition("" + hour));
+        dailyWordMin.setSelection(((ArrayAdapter<String>) dailyWordMin.getAdapter()).getPosition("" + minute));
+    }
+
+    private void addValueToTimeSpinner(Spinner hourSpinner, Spinner minuteSpinner, Context context) {
+        //add value to time spinner
+        ArrayList<String> listHour = new ArrayList<>();
+        ArrayList<String> listMinute = new ArrayList<>();
+        if (listHour.isEmpty()) {
+            for (int i = 0; i < 24; i++) {
+                listHour.add("" + i);
+            }
+        }
+        if (listMinute.isEmpty()) {
+            for (int i = 0; i < 60; i++) {
+                listMinute.add("" + i);
+            }
+        }
+        ArrayAdapter<String> hourAdapter = new ArrayAdapter<String>(context,
+                android.R.layout.simple_spinner_item, listHour);
+        hourAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hourSpinner.setAdapter(hourAdapter);
+        ArrayAdapter<String> minuteAdapter = new ArrayAdapter<String>(context,
+                android.R.layout.simple_spinner_item, listMinute);
+        minuteAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        minuteSpinner.setAdapter(minuteAdapter);
+    }
+
+    private void setReminderSwitch(Context context, Switch reminderSwitch) {
+        // set reminder switch is checked or not when loading UI
+        reminderSwitch.setChecked(SharedPreferencesController.reminderIsOn(context));
+    }
+
+    private void setDailyWordSwitch(Context context, Switch dailyWordSwitch) {
+        //set daily word switch is checked or not when load UI
+        dailyWordSwitch.setChecked(SharedPreferencesController.dailyWordIsOn(context));
+    }
+
+    private void setRef(View view) {
+        dailyWordSwitch = view.findViewById(R.id.dailyWordIsOn);
+        dailyWordHour = view.findViewById(R.id.dailyWordHour);
+        dailyWordMin = view.findViewById(R.id.dailyWordMinute);
+        reminderSwitch = view.findViewById(R.id.reminderIsOn);
+        reminderHour = view.findViewById(R.id.reminderHour);
+        reminderMin = view.findViewById(R.id.reminderMinute);
+        btnSave = view.findViewById(R.id.btnSaveSetting);
+        btnCancel = view.findViewById(R.id.btnCancelSetting);
+        dailyWordDismissSwitch = view.findViewById(R.id.dailyWordDisMissIsOn);
+        reminderDismissSwitch = view.findViewById(R.id.reminderDisMissIsOn);
     }
 
     @Override
