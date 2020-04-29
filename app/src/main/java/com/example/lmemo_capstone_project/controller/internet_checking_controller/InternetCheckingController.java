00f21cf9bfbc638097ceb57d472906d4212bdf8c
Hoang-Khai <khainh1998@gmail.com>
2020-04-29 23:27:51 +0700

    Fix check internet

--- app/src/main/java/com/example/lmemo_capstone_project/controller/internet_checking_controller/InternetCheckingController.java
+++ app/src/main/java/com/example/lmemo_capstone_project/controller/internet_checking_controller/InternetCheckingController.java
@@ -1,8 +1,8 @@
 package com.example.lmemo_capstone_project.controller.internet_checking_controller;
 
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+
+import java.io.IOException;
 
 public class InternetCheckingController {
     /**
@@ -10,19 +10,30 @@ public class InternetCheckingController {
      * この関数はインタネットとモバイルデータをチェックします
      */
     public static boolean isOnline (Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if (wifi != null) {
-            if (wifi.isConnected()) {
-                return true;
-            }
-        }
-        if (mobile != null) {
-            if (mobile.isConnected()) {
-                return true;
-            }
+//        ConnectivityManager connectivityManager = (ConnectivityManager) context
+//                .getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+//        NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+//        if (wifi != null) {
+//            if (wifi.isConnected()) {
+//                return true;
+//            }
+//        }
+//        if (mobile != null) {
+//            if (mobile.isConnected()) {
+//                return true;
+//            }
+//        }
+
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return false;
     }
